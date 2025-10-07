ccf00528df9e7ecf6e35024f5e08dc0a4197fd0f
SOCO PWA <juan@socopwa.com>
2025-10-07 12:51:59 -0700

    the den.live v1 code
    the den v1 code

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import TheDen_Landing_PixelPerfect from './TheDen_Landing_PixelPerfect';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TheDen_Landing_PixelPerfect />
+  </React.StrictMode>
+);
